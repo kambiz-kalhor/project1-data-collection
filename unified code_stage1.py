c4d1156733439f3d932c351f34d3c59c26bf8dd0
kambiz kalhor <kz.kalhor@gmail.com>
2021-03-13 23:24:47 +0330

    I added the 2nd and 3rd step and improved the code
    + cleaning data
    + webscraping to extract temperature data

--- unified code_stage1.py
+++ unified code_stage1.py
@@ -3,13 +3,22 @@
 
 # # importing all the packages we need
 
-# In[194]:
+# In[7]:
 
 
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import re
 
 
-# # read stage1_step1_export_bacdive_iso_table before cleaning.csv
+# # STEP ONE download a table from BacDive
+
+# i did it and the result is saved as stage1_step1_export_bacdive_iso_table before cleaning.csv
+
+# # STEP TWO
+
+# read stage1_step1_export_bacdive_iso_table before cleaning.csv
 
 # In[195]:
 
@@ -115,8 +124,117 @@ bacDive
 bacDive.to_csv(r'C:\Users\kamy\Desktop\stage1_step2_export_bacdive_iso_table cleaned.csv')
 
 
-# In[ ]:
+# # STEP THREE
+
+# WEB SCRAPING from BacDive
+
+# In[102]:
 
 
+bacDive = pd.read_csv(r"C:\Users\kamy\Desktop\stage1_step2_export_bacdive_iso_table cleaned.csv")
 
+
+# In[103]:
+
+
+bacDive
+
+
+# In[104]:
+
+
+# we want to creat URLs using the BacDive IDs
+all_IDs = bacDive['ID']
+
+#######################  reducing the size   this step must be removed  ###############
+all_IDs = all_IDs[0:10]
+######################################################################################
+
+
+# producing links for web scrapping
+
+# In[105]:
+
+
+def get_ID_give_URL(ID):
+    url = 'https://bacdive.dsmz.de/strain/' + str(ID)
+    return url
+
+
+# In[106]:
+
+
+def read_html(url):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.text
+    return None
+#CODE = 200 means the url is availible
+
+
+# In[107]:
+
+
+# in future i should improve this regex # its a regex to find temperature Data
+my_regex = re.compile("(Ref\.\:.\#\d+)\]\<\/a\>\<\/td\>\s\<td\>\<\/td\>\s\<td.class\=\"border\_rightfree\ textalign\_right\"\>\<\/td\>\s\<td.class\=\"border\_leftfree\"\>(\w+)\<\/td\>\s\<td.class\=\"border_leftfree textalign_center\"\>(\d{2}\-\d{2}|\d{2}\.\d{1}|\d{2})")
+
+
+# In[108]:
+
+
+my_data_frame = pd.DataFrame()
+
+
+for ID in all_IDs:
+    url = get_ID_give_URL(ID)
+    html_doc = read_html(url)
+    
+    
+    if html_doc is None:
+        print("Something went wrong!!!  the following url seems to be wrong   ; " , url)
+    
+    soup = BeautifulSoup(html_doc, "lxml")
+    list_of_extracted_data = []
+    
+    
+    #first step ==> extracting phylogeny data
+    tag = "valigntop paddingright"
+    data = soup.find_all("td", class_= tag)
+    for td in data:
+        list_of_extracted_data.append(td.text)
+        if len(list_of_extracted_data) == 9:
+            break
+        
+    #second step ==> extracting temp data
+    soup = str(soup)
+    temperature_data = my_regex.findall(soup)
+    list_of_extracted_data = list_of_extracted_data +temperature_data
+    while len(list_of_extracted_data) != 20:
+        list_of_extracted_data.append("")
+    my_data_frame[ID] = pd.Series(list_of_extracted_data)
+    
+   
+
+
+# transpose the dataframe
+my_data_frame = my_data_frame.T
+# naming columns
+my_data_frame = my_data_frame.rename(columns={0: 'Last LPSN update', 1: 'Domain', 2: 'Phylum', 3: 'Class', 4: 'Order', 5: 'Family', 6: 'Genus', 7: 'Species', 8: 'temperature Ref 1', 9: 'temperature Ref 2', 10: 'temperature Ref 3', 11: 'temperature Ref 4', 12: 'temperature Ref 5', 13: 'temperature Ref 6', 14: 'temperature Ref 7'})
+
+
+# saving the data
+
+# In[54]:
+
+
+my_data_frame.to_csv(r'C:\Users\kamy\Desktop\webscrapping_output.csv')
+
+
+# or you can put data together
+
+# In[111]:
+
+
+# i will do it later in my free time
 
